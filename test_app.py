1f6c2fa0f3da540a864f3141cc565bb1dfcf674f
Hakimu Jackson <hakimu@newrelic.com>
2014-12-07 20:03:13 -0800

    Update tutorial code

--- test_app.py
+++ test_app.py
@@ -1,9 +1,25 @@
+import random
+import string
+
 import cherrypy
 
 class HelloWorld(object):
-	@cherrypy.expose
-	def index(self):
-		return "Hello world!"
+  @cherrypy.expose
+  def index(self):
+    return """<html>
+      <head></head>
+      <body>
+        <form method="get" action="generate">
+          <input type="text" value="8" name="length" />
+          <button type="submit">Give it now!</button>
+        </form>
+      </body>
+    </html>"""      
+
+  @cherrypy.expose
+  def generate(self, length=8):
+    return ''.join(random.sample(string.hexdigits, int(length)))  
 
 if __name__ == '__main__':
-  cherrypy.quickstart(HelloWorld())		
+  cherrypy.quickstart(HelloWorld())    
+  
